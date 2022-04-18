f43a929f35f30a7b7f2ddf919463f3f8291e94a0
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 05:35:57 +0600

    models , get_absulate_url, Slugify

--- home/serializers.py
+++ home/serializers.py
@@ -6,3 +6,13 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+    # def validate(self, data):
+    #     if data['age'] > 18:
+    #         raise serializers.ValidationError(
+    #             {'error': 'Age can`t less then 18'})
+    #     if data['name']:
+    #         for n in data['name']:
+    #             if n.isdigit():
+    #                 raise serializers.ValidationError(
+    #                     {'error': 'Name Can not be  Numarical'})
