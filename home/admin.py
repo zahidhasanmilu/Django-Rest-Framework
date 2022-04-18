068c8982491770639d7bbe1c18d38e88176751b3
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 05:33:08 +0600

    RegisterStudent , Gender, Department models

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Student, Gender
+from .models import Student, Gender, Department
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Gender)
+admin.site.register(Department)
