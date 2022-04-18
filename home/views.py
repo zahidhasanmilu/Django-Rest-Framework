e2d42c23ccf7ffba080c004bf5113808181ef457
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-18 16:12:02 +0600

    api view POST

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+from home import serializers
 # Create your views here.
 
 
@@ -11,3 +12,13 @@ def home(request):
     student_info = Student.objects.all()
     serializers = StudentSerializer(student_info, many=True)
     return Response({'status': 200, 'payload': serializers.data})
+
+
+@api_view(['POST'])
+def student_post(request):
+    data = request.data
+    serializer = StudentSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response({'status': 403, 'message': 'Somhting Wrong'})
+    serializer.save()
+    return Response({'status': 200, 'payload': serializer, 'message': 'Post Sent'})
