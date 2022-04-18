68b5b72a253e7a6a763cc4a7e827093d88621832
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 05:34:54 +0600

    CRUD urls

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.students_info),
+    path('<int:id>/', views.student_info, name='student_info'),
     path('post/', views.student_post),
+    path('delete/<int:id>/', views.delete_student),
+    path('update/<int:id>/', views.update_student_info),
+
 ]
