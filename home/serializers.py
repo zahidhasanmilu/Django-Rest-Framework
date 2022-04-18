3469d8d252363de72c61796b8c2b275362447906
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 05:39:15 +0600

    Student Serializer

--- home/serializers.py
+++ home/serializers.py
@@ -7,12 +7,12 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Student
         fields = '__all__'
 
-    def validate(self, data):
-        if data['age'] > 18:
-            raise serializers.ValidationError(
-                {'error': 'Age can`t less then 18'})
-        if data['name']:
-            for n in data['name']:
-                if n.isdigit():
-                    raise serializers.ValidationError(
-                        {'error': 'Name Can not be  Numarical'})
+    # def validate(self, data):
+    #     if data['age'] > 18:
+    #         raise serializers.ValidationError(
+    #             {'error': 'Age can`t less then 18'})
+    #     if data['name']:
+    #         for n in data['name']:
+    #             if n.isdigit():
+    #                 raise serializers.ValidationError(
+    #                     {'error': 'Name Can not be  Numarical'})
